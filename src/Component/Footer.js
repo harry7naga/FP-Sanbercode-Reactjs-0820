5d8819e056a157a2e649709d358273fd91ae59e8
harry7naga <h4ry7naga@gmail.com>
2020-09-18 13:15:55 +0700

    Update Final Project

---
+++ src/Component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+
+const Footer = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Text className="mx-auto">Copyright &copy; 2020 by Hariono Mahalluddin</Navbar.Text>
+        </Navbar>
+    )
+}
+
+export default Footer
